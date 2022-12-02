76e834b196fdbf522bce20565c66bb21ee472eeb
Benjamin Fry <benjaminfry@me.com>
2022-12-02 15:24:42 -0800

    day 2 part 2 complete

--- days/2/src/main.rs
+++ days/2/src/main.rs
@@ -1,4 +1,3 @@
-use std::cmp::Ordering;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -27,6 +26,33 @@ enum Rochambeau {
     Scissors = 3,
 }
 
+#[derive(Clone, Copy)]
+#[repr(usize)]
+enum End {
+    Win,
+    Lose,
+    Draw,
+}
+
+impl FromStr for End {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let r = match s
+            .chars()
+            .next()
+            .expect("expected a character in the string")
+        {
+            'X' => End::Lose,
+            'Y' => End::Draw,
+            'Z' => End::Win,
+            r => panic!("Not an End: {}", r),
+        };
+
+        Ok(r)
+    }
+}
+
 impl Rochambeau {
     fn play(self, opponent: Rochambeau) -> usize {
         match (self, opponent) {
@@ -39,6 +65,20 @@ impl Rochambeau {
             (Rochambeau::Scissors, Rochambeau::Rock) => LOSS,
             (Rochambeau::Scissors, Rochambeau::Paper) => WIN,
             (Rochambeau::Scissors, Rochambeau::Scissors) => DRAW,
+        }
+    }
+
+    fn choose_for_end(self, end: End) -> Self {
+        match (self, end) {
+            (Rochambeau::Rock, End::Draw) => Rochambeau::Rock,
+            (Rochambeau::Rock, End::Lose) => Rochambeau::Scissors,
+            (Rochambeau::Rock, End::Win) => Rochambeau::Paper,
+            (Rochambeau::Paper, End::Win) => Rochambeau::Scissors,
+            (Rochambeau::Paper, End::Draw) => Rochambeau::Paper,
+            (Rochambeau::Paper, End::Lose) => Rochambeau::Rock,
+            (Rochambeau::Scissors, End::Lose) => Rochambeau::Paper,
+            (Rochambeau::Scissors, End::Win) => Rochambeau::Rock,
+            (Rochambeau::Scissors, End::Draw) => Rochambeau::Scissors,
         }
     }
 }
@@ -69,6 +109,11 @@ fn score_round(opponent: Rochambeau, player: Rochambeau) -> usize {
     score
 }
 
+fn score_round_for_end(opponent: Rochambeau, end: End) -> usize {
+    let player = opponent.choose_for_end(end);
+    score_round(opponent, player)
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Day 2");
     let args = Cli::parse();
@@ -80,10 +125,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let score = file
         .lines()
         .map(|s| s.expect("i/o error"))
-        .enumerate()
-        .filter(|(_, s)| !s.is_empty())
-        .map(|(line, s)| {
-            print!("iter: {line} play: {s} ");
+        .filter(|s| !s.is_empty())
+        .map(|s| {
             let mut split = s.split(' ');
             (
                 Rochambeau::from_str(split.next().expect("no opponent play"))
@@ -95,12 +138,31 @@ fn main() -> Result<(), Box<dyn Error>> {
         .fold(0usize, |mut score, (opponent, player)| {
             let play = score_round(opponent, player);
             score += play;
-            println!("play: {play} tally: {score}");
             score
         });
 
-    println!("Total score: {score}");
+    println!("Total score, part 1: {score}");
 
+    // part 2
+    let file = BufReader::new(File::open(filename)?);
+
+    let score = file
+        .lines()
+        .map(|s| s.expect("i/o error"))
+        .filter(|s| !s.is_empty())
+        .map(|s| {
+            let mut split = s.split(' ');
+            (
+                Rochambeau::from_str(split.next().expect("no opponent play"))
+                    .expect("not a Rochambeau"),
+                End::from_str(split.next().expect("no player play")).expect("not a Rochambeau"),
+            )
+        })
+        .fold(0usize, |score, (opponent, end)| {
+            score + score_round_for_end(opponent, end)
+        });
+
+    println!("Total score, part 2: {score}");
     Ok(())
 }
 
@@ -109,9 +171,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_example_data() {
+    fn test_example_data_part_1() {
         assert_eq!(score_round(Rochambeau::Rock, Rochambeau::Paper), 8);
         assert_eq!(score_round(Rochambeau::Paper, Rochambeau::Rock), 1);
         assert_eq!(score_round(Rochambeau::Scissors, Rochambeau::Scissors), 6);
+    }
+
+    #[test]
+    fn test_example_data_part_2() {
+        assert_eq!(score_round_for_end(Rochambeau::Rock, End::Draw), 4);
+        assert_eq!(score_round_for_end(Rochambeau::Paper, End::Lose), 1);
+        assert_eq!(score_round_for_end(Rochambeau::Scissors, End::Win), 7);
     }
 }
