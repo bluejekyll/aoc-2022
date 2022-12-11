6abfc500dc778a9a08aeefd5b6b93e73ef289bdc
Benjamin Fry <benjaminfry@me.com>
2022-12-11 11:23:07 -0800

    nieve day-11 part 2, overflows

--- src/bin/day-11.rs
+++ src/bin/day-11.rs
@@ -210,6 +210,89 @@
 //!
 //! Figure out which monkeys to chase by counting how many items they inspect over 20 rounds. What is the level of monkey business after 20 rounds of stuff-slinging simian shenanigans?
 //!
+//! --- Part Two ---
+//! You're worried you might not ever get your items back. So worried, in fact, that your relief that a monkey's inspection didn't damage an item no longer causes your worry level to be divided by three.
+//!
+//! Unfortunately, that relief was all that was keeping your worry levels from reaching ridiculous levels. You'll need to find another way to keep your worry levels manageable.
+//!
+//! At this rate, you might be putting up with these monkeys for a very long time - possibly 10000 rounds!
+//!
+//! With these new rules, you can still figure out the monkey business after 10000 rounds. Using the same example above:
+//!
+//! == After round 1 ==
+//! Monkey 0 inspected items 2 times.
+//! Monkey 1 inspected items 4 times.
+//! Monkey 2 inspected items 3 times.
+//! Monkey 3 inspected items 6 times.
+//!
+//! == After round 20 ==
+//! Monkey 0 inspected items 99 times.
+//! Monkey 1 inspected items 97 times.
+//! Monkey 2 inspected items 8 times.
+//! Monkey 3 inspected items 103 times.
+//!
+//! == After round 1000 ==
+//! Monkey 0 inspected items 5204 times.
+//! Monkey 1 inspected items 4792 times.
+//! Monkey 2 inspected items 199 times.
+//! Monkey 3 inspected items 5192 times.
+//!
+//! == After round 2000 ==
+//! Monkey 0 inspected items 10419 times.
+//! Monkey 1 inspected items 9577 times.
+//! Monkey 2 inspected items 392 times.
+//! Monkey 3 inspected items 10391 times.
+//!
+//! == After round 3000 ==
+//! Monkey 0 inspected items 15638 times.
+//! Monkey 1 inspected items 14358 times.
+//! Monkey 2 inspected items 587 times.
+//! Monkey 3 inspected items 15593 times.
+//!
+//! == After round 4000 ==
+//! Monkey 0 inspected items 20858 times.
+//! Monkey 1 inspected items 19138 times.
+//! Monkey 2 inspected items 780 times.
+//! Monkey 3 inspected items 20797 times.
+//!
+//! == After round 5000 ==
+//! Monkey 0 inspected items 26075 times.
+//! Monkey 1 inspected items 23921 times.
+//! Monkey 2 inspected items 974 times.
+//! Monkey 3 inspected items 26000 times.
+//!
+//! == After round 6000 ==
+//! Monkey 0 inspected items 31294 times.
+//! Monkey 1 inspected items 28702 times.
+//! Monkey 2 inspected items 1165 times.
+//! Monkey 3 inspected items 31204 times.
+//!
+//! == After round 7000 ==
+//! Monkey 0 inspected items 36508 times.
+//! Monkey 1 inspected items 33488 times.
+//! Monkey 2 inspected items 1360 times.
+//! Monkey 3 inspected items 36400 times.
+//!
+//! == After round 8000 ==
+//! Monkey 0 inspected items 41728 times.
+//! Monkey 1 inspected items 38268 times.
+//! Monkey 2 inspected items 1553 times.
+//! Monkey 3 inspected items 41606 times.
+//!
+//! == After round 9000 ==
+//! Monkey 0 inspected items 46945 times.
+//! Monkey 1 inspected items 43051 times.
+//! Monkey 2 inspected items 1746 times.
+//! Monkey 3 inspected items 46807 times.
+//!
+//! == After round 10000 ==
+//! Monkey 0 inspected items 52166 times.
+//! Monkey 1 inspected items 47830 times.
+//! Monkey 2 inspected items 1938 times.
+//! Monkey 3 inspected items 52013 times.
+//! After 10000 rounds, the two most active monkeys inspected items 52166 and 52013 times. Multiplying these together, the level of monkey business in this situation is now 2713310158.
+//!
+//! Worry levels are no longer divided by three after each item is inspected; you'll need to find another way to keep your worry levels manageable. Starting again from the initial state in your puzzle input, what is the level of monkey business after 10000 rounds?
 
 use std::error::Error;
 use std::fmt::Write;
@@ -284,7 +367,7 @@ impl Instruction {
     fn inspection_score(&self, worry: usize) -> usize {
         let arg1 = match self.arg1 {
             Literal::Num(val) => val,
-            Literal::Old => worry,
+            Literal::Old => worry.clone(),
         };
         let arg2 = match self.arg2 {
             Literal::Num(val) => val,
@@ -420,9 +503,9 @@ fn parse_monkeys(input: &str) -> Vec<Monkey> {
     monkeys
 }
 
-fn monkey_business(monkeys: &mut [Monkey]) {
+fn monkey_business(monkeys: &mut [Monkey], rounds: usize, reduce_anxiety: bool) {
     // rounds
-    for _ in 0..20 {
+    for _ in 0..rounds {
         // (target index, item worry score)
         let mut items_thrown_to = Vec::<(usize, usize)>::new();
 
@@ -437,11 +520,13 @@ fn monkey_business(monkeys: &mut [Monkey]) {
                 for worry in monkey.items.drain(..) {
                     monkey.inspected_items_count += 1;
                     // raise the anxiety
-                    let new_worry = anxiety.inspection_score(worry);
+                    let mut new_worry = anxiety.inspection_score(worry);
                     // wow, it's still ok, divide by 3
-                    let new_worry = new_worry / 3;
+                    if reduce_anxiety {
+                        new_worry /= 3_usize;
+                    }
 
-                    if new_worry % test.divisor == 0 {
+                    if &new_worry % test.divisor == 0_usize {
                         items_thrown_to.push((monkey.test.true_monkey, new_worry));
                     } else {
                         items_thrown_to.push((monkey.test.false_monkey, new_worry));
@@ -487,10 +572,16 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // got all the monkeys
     let mut monkeys = parse_monkeys(&input);
-    monkey_business(&mut monkeys);
+    monkey_business(&mut monkeys, 20, true);
 
     let total_mb = calculate_top_monkey_bussiness(&monkeys);
     println!("part 1, monkey business product: {total_mb}");
+
+    let mut monkeys = parse_monkeys(&input);
+    monkey_business(&mut monkeys, 10000, false);
+
+    let total_mb = calculate_top_monkey_bussiness(&monkeys);
+    println!("part 2, monkey business product: {total_mb}");
 
     Ok(())
 }
@@ -531,10 +622,10 @@ Monkey 3:
 
     #[test]
     fn test_parse_items() {
-        assert_eq!(parse_items("  Starting items: 74").unwrap().1, &[74]);
+        assert_eq!(parse_items("  Starting items: 74").unwrap().1, &[74usize]);
         assert_eq!(
             parse_items("  Starting items: 79, 60, 97").unwrap().1,
-            &[79, 60, 97]
+            &[79usize, 60usize, 97usize]
         );
     }
 
@@ -567,7 +658,7 @@ Monkey 3:
     fn test_part1_input() {
         let mut monkeys = parse_monkeys(INPUT);
 
-        monkey_business(&mut monkeys);
+        monkey_business(&mut monkeys, 20, true);
 
         assert_eq!(monkeys[0].inspected_items_count, 101);
         assert_eq!(monkeys[1].inspected_items_count, 95);
@@ -575,5 +666,19 @@ Monkey 3:
         assert_eq!(monkeys[3].inspected_items_count, 105);
 
         assert_eq!(calculate_top_monkey_bussiness(&monkeys), 10605);
+    }
+
+    #[test]
+    fn test_part2_input() {
+        let mut monkeys = parse_monkeys(INPUT);
+
+        monkey_business(&mut monkeys, 10000, false);
+
+        assert_eq!(monkeys[0].inspected_items_count, 52166);
+        assert_eq!(monkeys[1].inspected_items_count, 47830);
+        assert_eq!(monkeys[2].inspected_items_count, 1938);
+        assert_eq!(monkeys[3].inspected_items_count, 52013);
+
+        assert_eq!(calculate_top_monkey_bussiness(&monkeys), 2713310158);
     }
 }
