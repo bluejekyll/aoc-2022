60e7e00e2e2299dfb5777864efc33dfe1459daa4
Benjamin Fry <benjaminfry@me.com>
2022-12-13 22:14:37 -0800

    day 13 part 1 complete

--- src/bin/day-13.rs
+++ src/bin/day-13.rs
@@ -107,6 +107,39 @@
 //!
 //! Determine which pairs of packets are already in the right order. What is the sum of the indices of those pairs?
 //!
+//! --- Part Two ---
+//! Now, you just need to put all of the packets in the right order. Disregard the blank lines in your list of received packets.
+//!
+//! The distress signal protocol also requires that you include two additional divider packets:
+//!
+//! [[2]]
+//! [[6]]
+//! Using the same rules as before, organize all packets - the ones in your list of received packets as well as the two divider packets - into the correct order.
+//!
+//! For the example above, the result of putting the packets in the correct order is:
+//!
+//! []
+//! [[]]
+//! [[[]]]
+//! [1,1,3,1,1]
+//! [1,1,5,1,1]
+//! [[1],[2,3,4]]
+//! [1,[2,[3,[4,[5,6,0]]]],8,9]
+//! [1,[2,[3,[4,[5,6,7]]]],8,9]
+//! [[1],4]
+//! [[2]]
+//! [3]
+//! [[4,4],4,4]
+//! [[4,4],4,4,4]
+//! [[6]]
+//! [7,7,7]
+//! [7,7,7,7]
+//! [[8,7,6]]
+//! [9]
+//! Afterward, locate the divider packets. To find the decoder key for this distress signal, you need to determine the indices of the two divider packets and multiply them together. (The first packet is at index 1, the second packet is at index 2, and so on.) In this example, the divider packets are 10th and 14th, and so the decoder key is 140.
+//!
+//! Organize all of the packets into the correct order. What is the decoder key for the distress signal?
+//!
 
 use std::cmp::Ordering;
 use std::error::Error;
@@ -148,35 +181,21 @@ impl Packet {
         match (self, other) {
             (Packet::Literal(this), Packet::Literal(that)) => this.cmp(that),
             (this @ Packet::Literal(_), other @ Packet::List(_list)) => {
-                println!("=LIST= {this:?} =OO= {other:?}");
                 Packet::List(vec![this.clone()]).is_lesser(other)
             }
             (this @ Packet::List(_), other @ Packet::Literal(_)) => {
-                println!("=SS= {this:?} =LIST= {other:?}");
                 this.is_lesser(&Packet::List(vec![other.clone()]))
             }
             (Packet::List(these), Packet::List(those)) => {
-                println!("=SS= {these:?} =OO= {those:?}");
-
-                let mut order = Ordering::Equal;
                 for (this, that) in these.iter().zip(those.iter()) {
                     match this.is_lesser(that) {
                         Ordering::Greater => return Ordering::Greater,
-                        ordering => {
-                            // only set if it's eq, if it's lesser, we want it to remain so
-                            if order.is_eq() {
-                                order = ordering
-                            }
-                        }
+                        Ordering::Less => return Ordering::Less,
+                        Ordering::Equal => (), // continue
                     }
                 }
 
-                if !order.is_gt() && these.len() < those.len() {
-                    return Ordering::Less;
-                } else if !order.is_lt() && these.len() > those.len() {
-                    return Ordering::Greater;
-                }
-                order
+                these.len().cmp(&those.len())
             }
         }
     }
